43278659e09b308db04b6908c1b23ad3d3972cf1
Mike Moore <mike@blowmage.com>
2013-10-25 04:44:26 -0600

    Improved GroupsHelper test coverage

--- test/helpers/groups_helper_test.rb
+++ test/helpers/groups_helper_test.rb
@@ -3,16 +3,38 @@ require 'test_helper'
 class GroupsHelperTest < ActionView::TestCase
 
   def current_user
-    users :mike
+    @current_user
   end
 
   def setup
     @group = groups :payroll
+    @current_user = users :mike
   end
 
-  def test_is_member?
+  def test_is_member_member
     assert @group.is_member? current_user
     assert is_member?
   end
 
+  def test_is_member_member
+    @current_user = users :zach
+    refute @group.is_member? current_user
+    refute is_member?
+  end
+
+  def test_is_member_no_user
+    @current_user = nil
+    refute is_member?
+  end
+
+  def test_is_member_no_group
+    @group = nil
+    refute is_member?
+  end
+
+  def test_is_member_no_user_no_group
+    @group = nil
+    @current_user = nil
+    refute is_member?
+  end
 end
